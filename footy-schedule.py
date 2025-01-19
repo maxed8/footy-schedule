ca4b157acaf8cf4ef887f38877ff2829188bf4c3
maxed8 <maxedelstein.8@gmail.com>
2025-01-19 17:21:45 -0500

    filtering

--- footy-schedule.py
+++ footy-schedule.py
@@ -1,7 +1,7 @@
 import requests
 
 competitions = {
-    'FA Cup': 'FAC',
+    # 'FA Cup': 'FAC',
     'Premier League': 'PL',
     'Champions League': 'CL',
     'Serie A': 'SA',
@@ -10,74 +10,93 @@ competitions = {
     'Ligue 1': 'FL1'
 }
 
-premTeams = {
-    'Manchester United FC',
-    'Chelsea FC',
-    'Arsenal FC',
-    'Manchester City FC',
-    'Tottenham Hotspur FC',
-    'Liverpool FC',
-    'Aston Villa FC',
-    'Nottingham Forest FC',
-    'Brighton & Hove Albion FC'
-}
-
-uclTeams = {
-    'FC Barcelona',
-    'Real Madrid CF',
-    'Paris Saint-Germain FC',
-    'Bayer 04 Leverkusen',
-    'Borussia Dortmund',
-    'FC Bayern München',
-    'AC Milan',
-    'FC Internazionale Milano',
-    'Arsenal FC',
-    'Manchester City FC',
-    'Liverpool FC',
-    'Aston Villa FC'
-}
-
-laLigaTeams = {
-    'FC Barcelona',
-    'Real Madrid CF',
-    'Club Atlético de Madrid'
-}
-
-bundesligaTeams = {
-    'Bayer 04 Leverkusen',
-    'Borussia Dortmund',
-    'FC Bayern München'
-}
-
-serieATeams = {
-    'AC Milan',
-    'FC Internazionale Milano',
-    'Juventus FC',
-    'Atalanta BC'
-}
-
-ligueOneTeams = {
-    'Paris Saint-Germain FC',
-    'Olympique de Marseille'
+teams = {
+    'Premier League': {
+        'Manchester United FC',
+        'Chelsea FC',
+        'Arsenal FC',
+        'Manchester City FC',
+        'Tottenham Hotspur FC',
+        'Liverpool FC',
+        'Aston Villa FC',
+        'Nottingham Forest FC',
+        'Brighton & Hove Albion FC'
+    },
+    'Champions League': {
+        'FC Barcelona',
+        'Real Madrid CF',
+        'Paris Saint-Germain FC',
+        'Bayer 04 Leverkusen',
+        'Borussia Dortmund',
+        'FC Bayern München',
+        'AC Milan',
+        'FC Internazionale Milano',
+        'Arsenal FC',
+        'Manchester City FC',
+        'Liverpool FC',
+        'Aston Villa FC'
+    },
+    'La Liga': {
+        'FC Barcelona',
+        'Real Madrid CF',
+        'Club Atlético de Madrid'
+    },
+    'Bundesliga': {
+        'Bayer 04 Leverkusen',
+        'Borussia Dortmund',
+        'FC Bayern München'
+    },
+    'Serie A': {
+        'AC Milan',
+        'FC Internazionale Milano',
+        'SSC Napoli',
+        'Juventus FC',
+        'Atalanta BC'
+    },
+    'Ligue 1': {
+        'Paris Saint-Germain FC',
+        'Olympique de Marseille'
+    }
 }
 
 
 headers = { 'X-Auth-Token': '66354c8ea27b4b57904f7bd5541fc6f6' }
 
+schedule = {key: [] for key in competitions.keys()}
+
+# TODO: figure out this week's matchweek for each league
+
 for name, code in competitions.items():
-    if code in {'FAC', 'CL'}:
+    if code == 'CL':
         uri = f'https://api.football-data.org/v4/competitions/{code}/matches'
     else:
         uri = f'https://api.football-data.org/v4/competitions/{code}/matches?matchday=22'
+
     response = requests.get(uri, headers=headers)
-    print(name)
-    if code != 'CL' and code != 'FAC':
+    data = response.json()
+
+    if 'matches' in data:
         for match in response.json()['matches']:
-            print(match['homeTeam']['name'] + ' vs. ' + match['awayTeam']['name'])
-        print('------------------------------------')
+            if code == 'CL':
+                if match['matchday'] == 7:
+                    schedule[name].append((match['homeTeam']['name'], match['awayTeam']['name'], match['utcDate']))
+            else:
+                schedule[name].append((match['homeTeam']['name'], match['awayTeam']['name'], match['utcDate']))
     else:
-        print(response.json)
-        print('------------------------------------')
+        print(f'Error: API request failed with status code {response.status_code}')
+    
+
+    
+# print(matches)
+
+print('Good Games')
+print('-----------------')
+for comp, matches in schedule.items():
+    for match in matches:
+        if match[0] in teams[comp] and match[1] in teams[comp]:
+            print(match[0] + ' vs. ' + match[1] + ' at ' + match[2])
+
+
 
 
 
--- playground.py
+++ playground.py
@@ -1,20 +1,20 @@
 import requests
 
-# uri = 'https://api.football-data.org/v4/competitions/CL/matches'
-# headers = { 'X-Auth-Token': '66354c8ea27b4b57904f7bd5541fc6f6' }
-# response = requests.get(uri, headers=headers)
-# resp_data = response.json()
-
-# for match in resp_data['matches']:
-#     print(match['matchday'])
-#     print(match['homeTeam']['name'] + ' vs. ' + match['awayTeam']['name'])
-
-
-uri = 'https://api.football-data.org/v4/competitions/FAC/matches'
+uri = 'https://api.football-data.org/v4/competitions/CL/matches'
 headers = { 'X-Auth-Token': '66354c8ea27b4b57904f7bd5541fc6f6' }
 response = requests.get(uri, headers=headers)
 resp_data = response.json()
-print(resp_data)
+
+for match in resp_data['matches']:
+    print(match['matchday'])
+    print(match['homeTeam']['name'] + ' vs. ' + match['awayTeam']['name'])
+
+
+# uri = 'https://api.football-data.org/v4/competitions/FAC/matches'
+# headers = { 'X-Auth-Token': '66354c8ea27b4b57904f7bd5541fc6f6' }
+# response = requests.get(uri, headers=headers)
+# resp_data = response.json()
+# print(resp_data)
 
 # for match in resp_data['matches']:
 #     print(match['matchday'])
